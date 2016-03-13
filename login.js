e9d08bc2ce5a4caa9b36c0b440901bbc13911fdc
swethaturumalla <turumallaswetha@gmail.com>
2016-03-13 23:39:27 +0530

    Added files via upload

--- login.js
+++ login.js
@@ -1,22 +1,23 @@
-
 Session.setDefault('role', 'farmer');
 
-Template.pp-login-form.events({
-	'change [data-action="changeRole"]': function(event, template) {
-		var role = $('.farmer-register input[name="role"]').val();
+Template.login.events({
+	'change #role': function(event, template) {
+		console.log("inside change");
+		var role = $("input[name='role']:checked").val();
+	console.log("radio value"+role);
 		Session.set('role', role);		
 	},
 
 	    
-  'click [data-action="login"]': function(event, template) {
+  'click #login': function(event, template) {
   	event.preventDefault();
     
-  	var role = $('.farmer-register input[name="role"]').val();
-  	var userId = $('.farmer-register input[name="userId"]').val();
-  	var password = $('.farmer-register input[name="password"]').val();
-    
-    console.info(role);
-    if(role == 'farmer')
+  	var role = $("input[name='role']:checked").val();
+  	var userId = $("input[name='userId']").val();
+  	var password = $("input[name='password']").val();
+    console.log("role"+role+"name "+userId);
+	Session.set('currentUser',userId);
+    if(role === 'farmer')
     {
     	Meteor.call('retrieveFarmer', userId, function(error, result) {
     		if(result) {
@@ -24,13 +25,12 @@ Template.pp-login-form.events({
     			Session.set('currentUser', farmer)
     			Router.go('home'); //TODO: create home page
     		}
-    	});
-    }
-    else
-    {
-    	Router.go('vhome'); //TODO: create route and page
-    }
+    		else
+    		{
+    			Router.go('home'); //TODO: create route and e
+    		}
      
 	
-  }
+  	});
+}}
 });
